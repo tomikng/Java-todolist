4e4bb2e1550aa6e5890c364fc3e5e1d130cb3b6f
Hai Hung Nguyen <tomasnguyen43@gmail.com>
2024-02-25 17:44:18 +0100

    feat(priority): add edition priority

--- src/main/java/cz/nguyeha/todolist/controller/TaskDetailController.java
+++ src/main/java/cz/nguyeha/todolist/controller/TaskDetailController.java
@@ -1,8 +1,10 @@
 package cz.nguyeha.todolist.controller;
 
 import cz.nguyeha.todolist.database.DatabaseHelper;
+import cz.nguyeha.todolist.enums.Priority;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -18,6 +20,11 @@ public class TaskDetailController {
     @FXML
     private CheckBox completedCheckBox;
 
+    @FXML
+    private ComboBox<String> priorityComboBox;
+
+
+
     private Task task;
 
     public void setTask(Task task) {
@@ -26,6 +33,7 @@ public class TaskDetailController {
         datePicker.setValue(task.getDueDate());
         descriptionTextField.setText(task.getDescription());
         completedCheckBox.setSelected(task.isCompleted());
+        priorityComboBox.setValue(task.getPriority().toString());
     }
 
     @FXML
@@ -34,6 +42,7 @@ public class TaskDetailController {
         task.setDueDate(datePicker.getValue());
         task.setDescription(descriptionTextField.getText());
         task.setCompleted(completedCheckBox.isSelected());
+        task.setPriority(Priority.valueOf(priorityComboBox.getValue()));
 
         DatabaseHelper.updateTask(task);
 
